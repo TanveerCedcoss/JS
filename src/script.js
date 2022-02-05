8089027335c2487ea22273966cd16360f3e3a2cd
TanveerCedcoss <tanveerhusain@cedcoss.com>
2022-02-05 11:14:59 +0530

    Understanding Uninitialized Variables

--- src/script.js
+++ src/script.js
@@ -2,8 +2,15 @@
 /* This is my second
 comment*/
 var ourName;
-var a=9;
-var b;
-b=a
+//var a=9;
+//var b;
+//b=a
 var myFirstName="myFirstName";
 var myLastName="myLastName";
+var a=5;
+var b=10;
+var c="I am a";
+
+a = a + 1;
+b = b + 5;
+c = c + " String!";
